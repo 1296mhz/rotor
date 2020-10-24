4a4863bc2b77bfda017db856547c551afc1b451d
cshlovjah <1296mhz@gmail.com>
2020-10-24 23:01:15 +0500

    Последний коммит для UNO? делаю ветьку для меги

--- LCDShield.ino
+++ LCDShield.ino
@@ -14,9 +14,9 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 #define ANALOG_KEYS_PIN A0 // Шина для аналоговых кнопок A0
 #define ANALOG_KEYS_PIN_A5 A5 // Шина для аналоговых кнопок A5
 //петля усреднения
-const int numReadings = 25;
-int readIndex = 0;
-
+const int numReadings = 10;
+int readIndexAz = 0;
+int readIndexEl = 0;
 int azAngle = 0;          // Угол азимута
 int azOldSensorValue = 0; // Предыдущее значение с датчика азимута
 int azTarget = 300;       // Цель для поворота
@@ -90,22 +90,22 @@ int azSensor()
 {
 
   // AVERAGING LOOP - subtract the last reading:
-  totalAz = totalAz - azimuth[readIndex];
+  totalAz = totalAz - azimuth[readIndexAz];
   // Читакм сенсор
-  azimuth[readIndex] = analogRead(AZSENSOR);
+  azimuth[readIndexAz] = analogRead(AZSENSOR);
   // прибавьте показание к итогу
-  totalAz = totalAz + azimuth[readIndex];
+  totalAz = totalAz + azimuth[readIndexAz];
   // перейти к следующей позиции в массиве
-  readIndex = readIndex + 1;
+  readIndexAz = readIndexAz + 1;
   // если мы в конце массива ...
-  if (readIndex >= numReadings)
+  if (readIndexAz >= numReadings)
   {
     // ...еще раз
-    readIndex = 0;
+    readIndexAz = 0;
   }
   // calculate the average:
   averageAz = totalAz / numReadings;
-  //  Serial.println(averageAz);
+  // Serial.println(averageAz);
   azAngle = (averageAz - 86) * 1.127;
   azAngle = int(azAngle / 2.842); // azimuth value 0-359
   if (azAngle < 0)
@@ -123,29 +123,31 @@ int azSensor()
 int elSensor()
 {
   //ELEVATION AVERAGING LOOP
-  totalEl = totalEl - elevation[readIndex];
-  elevation[readIndex] = analogRead(ELSENSOR);
+  totalEl = totalEl - elevation[readIndexEl];
+  elevation[readIndexEl] = analogRead(ELSENSOR);
 
-  totalEl = totalEl + elevation[readIndex];
-  readIndex = readIndex + 1;
-  if (readIndex >= numReadings)
+  totalEl = totalEl + elevation[readIndexEl];
+  readIndexEl = readIndexEl + 1;
+  if (readIndexEl >= numReadings)
   {
-    readIndex = 0;
+    readIndexEl = 0;
   }
   averageEl = totalEl / numReadings;
-  //elAngle = (averageEl - 86) * 1.127;
-  //elAngle = int(elAngle / 2.842); // elevation value 0-359
-
-  elAngle = ((averageEl - 2) * 1.025);
+  elAngle = (averageEl - 86) * 1.127;
   elAngle = int(elAngle / 2.842); // elevation value 0-359
+  
+  //elAngle = ((averageEl-2)*1.025);
+  //elAngle = int(elAngle/11.3);       // elev value 0-90
   if (elAngle < 0)
   {
     elAngle = 0;
   }
-  if (elAngle > 359)
+  if (elAngle > 90)
   {
-    elAngle = 359;
+    elAngle = 90;
   }
+
+  return elAngle;
 }
 
 void getSensors()
@@ -157,9 +159,10 @@ void getSensors()
 void getKeysA0()
 {
   adcKeyIn = analogRead(ANALOG_KEYS_PIN);
-
-  adcKeyIn = getKey(adcKeyIn);
+    Serial.print("KEYA0: ");
   Serial.println(adcKeyIn);
+  adcKeyIn = getKey(adcKeyIn);
+
   if (adcKeyIn == 0)
   {
     delay(300);
@@ -199,9 +202,10 @@ void getKeysA0()
 void getKeysA5()
 {
   adcKeyIn = analogRead(ANALOG_KEYS_PIN_A5);
-
-  adcKeyIn = getKey(adcKeyIn);
+  Serial.print("KEYA5: ");
   Serial.println(adcKeyIn);
+  adcKeyIn = getKey(adcKeyIn);
+
   if (operate) {
     if (adcKeyIn == 3)
     {
@@ -362,6 +366,7 @@ void setup()
   pinMode(PIN_UP, OUTPUT);
   pinMode(PIN_DOWN, OUTPUT);
   pinMode(AZSENSOR, INPUT);
+  pinMode(ELSENSOR, INPUT);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
   lcd.createChar(1, heart);
@@ -415,6 +420,14 @@ void loop()
 
     if (elMove)
     {
+      if (elTarget == elAngle)
+      {
+        lcd.setCursor(15, 0);
+        lcd.print(" ");
+        digitalWrite(PIN_UP, LOW);
+        digitalWrite(PIN_DOWN, LOW);
+        elMove = false;
+      }
       if (elTarget - elAngle >= 1)
       {
         up(elMove);
@@ -423,11 +436,6 @@ void loop()
       if (elAngle - elTarget >= 1)
       {
         down(elMove);
-      }
-
-      if (elTarget == elAngle)
-      {
-        elMove = false;
       }
     }
   }
